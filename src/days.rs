fa5ee75e1b305d70b85aaaf39c227d5d8eef0004
David Philipson <david.philipson@gmail.com>
2024-12-19 00:15:21 -0500

    Day 19

--- src/days.rs
+++ src/days.rs
@@ -1,8 +1,8 @@
-pub mod day18;
+pub mod day19;
 
-pub use day18 as day;
+pub use day19 as day;
 
-pub const DAY: usize = 18;
+pub const DAY: usize = 19;
 
 // Uncomment below to compile all days instead of just the current one.
 
--- src/days/day19.rs
+++ src/days/day19.rs
@@ -1,9 +1,54 @@
 use crate::harness::input::RawInput;
 
 pub fn solve_part1(input: RawInput) -> usize {
-    todo!("{}", &input.as_str()[..0])
+    let (towels, goals) = parse(input);
+    goals
+        .into_iter()
+        .filter(|goal| count_ways_to_goal(&towels, goal, 0, &mut vec![None; goal.len()]) > 0)
+        .count()
 }
 
 pub fn solve_part2(input: RawInput) -> usize {
-    todo!("{}", &input.as_str()[..0])
+    let (towels, goals) = parse(input);
+    goals
+        .into_iter()
+        .map(|goal| count_ways_to_goal(&towels, &goal, 0, &mut vec![None; goal.len()]))
+        .sum()
+}
+
+fn parse(input: RawInput) -> (Vec<Vec<char>>, Vec<Vec<char>>) {
+    let (towels, goals) = input.split_once_on_empty_line();
+    let towels = towels
+        .single_line(|line| line.split::<String>(", "))
+        .into_iter()
+        .map(|s| s.chars().collect::<Vec<_>>())
+        .collect();
+    let goals = goals.per_line(|line| line.chars()).collect();
+    (towels, goals)
+}
+
+fn count_ways_to_goal(
+    towels: &[Vec<char>],
+    goal: &[char],
+    i: usize,
+    cache: &mut [Option<usize>],
+) -> usize {
+    if i == goal.len() {
+        return 1;
+    }
+    if let Some(cached) = cache[i] {
+        return cached;
+    }
+    let mut ways = 0;
+    for towel in towels {
+        if is_prefix(towel, &goal[i..]) {
+            ways += count_ways_to_goal(towels, goal, i + towel.len(), cache);
+        }
+    }
+    cache[i] = Some(ways);
+    ways
+}
+
+fn is_prefix(a: &[char], b: &[char]) -> bool {
+    a.len() <= b.len() && a.iter().zip(b).all(|(a, b)| a == b)
 }
